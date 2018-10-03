3ce4fbeaa993253de32fafaa4cc2f2bead6dc0d3
Prakash Bhattarai <prakash1bhattarai25@gmail.com>
2018-10-03 15:45:39 +0545

    Added exception handling in termbingrabber script

--- termbingrabber.sh
+++ termbingrabber.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 link="http://termbin.com/$1"
+if [ "$1" == "" ]
+then
+	echo "error occured: token not provided"
+	exit 1
+	#exit <status_code>
+fi
 if [ "$2" != "" ]
 then
 	curl -L "$link" > $2
